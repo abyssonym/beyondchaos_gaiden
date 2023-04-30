e7dfc9f13186f21c26480641865d7d8e616f1cdf
Anonymous <none>
2023-04-30 12:15:40 -0700

    some changes for compatibility with BCCE

--- bcg_junction.py
+++ bcg_junction.py
@@ -5,39 +5,46 @@ import json
 from os import path
 
 def populate_data(data, filename, address):
-    with open(filename, 'r+b') as f:
-        mindex = min(data)
-        maxdex = max(data)
-        assert mindex >= 0
-        pointer = address + (maxdex * 2) + 2
-        nothing_pointer = pointer
-        f.seek(nothing_pointer)
-        f.write(b'\x00')
-        pointer += 1
-        pointers = []
-        for i in range(maxdex+1):
-            if i not in data:
-                values = []
-            else:
-                values = sorted(data[i])
-            if not values:
-                pointers.append(nothing_pointer)
-                continue
-            assert 0 not in values
-            values = sorted(set(values))
-            values.append(0)
-            f.seek(pointer)
-            f.write(bytes(values))
-            pointers.append(pointer)
-            pointer += len(values)
+    if isinstance(filename, str):
+        f = open(filename, 'r+b')
+    else:
+        f = filename
 
-        assert len(pointers) == maxdex + 1
-        assert len({map_to_snes(pointer) >> 16 for pointer in pointers}) == 1
-        f.seek(address)
-        for pointer in pointers:
-            p = pointer & 0xFFFF
-            assert 0 <= p <= 0xFFFF
-            f.write(p.to_bytes(2, byteorder='little'))
+    mindex = min(data)
+    maxdex = max(data)
+    assert mindex >= 0
+    pointer = address + (maxdex * 2) + 2
+    nothing_pointer = pointer
+    f.seek(nothing_pointer)
+    f.write(b'\x00')
+    pointer += 1
+    pointers = []
+    for i in range(maxdex+1):
+        if i not in data:
+            values = []
+        else:
+            values = sorted(data[i])
+        if not values:
+            pointers.append(nothing_pointer)
+            continue
+        assert 0 not in values
+        values = sorted(set(values))
+        values.append(0)
+        f.seek(pointer)
+        f.write(bytes(values))
+        pointers.append(pointer)
+        pointer += len(values)
+
+    assert len(pointers) == maxdex + 1
+    assert len({map_to_snes(pointer) >> 16 for pointer in pointers}) == 1
+    f.seek(address)
+    for pointer in pointers:
+        p = pointer & 0xFFFF
+        assert 0 <= p <= 0xFFFF
+        f.write(p.to_bytes(2, byteorder='little'))
+
+    if isinstance(filename, str):
+        f.close()
 
 
 class JunctionManager:
