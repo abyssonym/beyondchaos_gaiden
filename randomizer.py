82dd06fa109a9ad8cc83bedfe726a046cc00e3e1
Anonymous <none>
2023-05-13 12:55:37 -0700

    add esper defense junction

--- randomizer.py
+++ randomizer.py
@@ -4159,6 +4159,7 @@ def test():
     jm.add_junction(0x13, 'repel_ice', force_category='monster')
     jm.add_junction('gau', 'immortal')
     jm.add_junction(None, 'catch')
+    jm.add_junction(None, 'esper_defense', 'whitelist')
     jm.execute()
     return jm
 
